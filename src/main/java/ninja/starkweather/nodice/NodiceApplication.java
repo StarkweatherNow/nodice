8a55d19f04ff25f890909ba5c65c1eacd205d098
StarkweatherNow <bstarkw@wgu.edu>
2023-10-25 09:02:37 -0400

    first commit

---
+++ src/main/java/ninja/starkweather/nodice/NodiceApplication.java
@@ -0,0 +1,13 @@
+package ninja.starkweather.nodice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodiceApplication.class, args);
+	}
+
+}
